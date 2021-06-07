23b8865ee8c1a09ced35c07ff3f488c747cb8604
Arianna Basha <ariannagbasha@gmail.com>
2021-06-07 14:58:14 -0500

    recursive function with refs;

--- Gabrielle_Basha_PWD_TA.js
+++ Gabrielle_Basha_PWD_TA.js
@@ -24,7 +24,7 @@ console.log(nonSedanBrands)
  * e.g., reverseString('cat') => 'tac'
  */
 
-const reverseString = (str) => {
+const reverseStringLooping = (str) => {
     let reversedStr = ''
     for(let i = str.length - 1; i >= 0; i--) {
       reversedStr += str[i]
@@ -32,7 +32,7 @@ const reverseString = (str) => {
     return reversedStr
 }
 
-console.log(reverseString('cat'))
+console.log(reverseStringLooping('cat'))
 
 // 3.
 // Using Reduce,
@@ -67,6 +67,46 @@ console.log(shop(wishlist));
 // A detailed explanation.
 
 
-// Recursion
 
-// Going back to 
+
+
+
+// Recursion is where a function calls itself
+
+// A recursive algorithm consists two fundamental parts:
+
+// 1. The base case where we stop the recursive call
+// 2. The recursive step, where we can call the function by making another call 
+
+// Say if you are reversing a string and the first thing that comes to mind when reversing is looping string backwards
+
+
+//  a part of the function call
+
+const reversedStringRecursion = (reversedStr) => {
+    //base step
+    // If you think about the base step of this is that reversed step is reversedStr = '', this is the beginning where we stop 
+    if( reversedStr === '') {
+      return ''
+    }
+
+    // Now what about the recursive step ? 
+    // in the prior step I had i start at i = str.length - 1 why ? Because I wanted start at the end, in this case, we will be starting at the beginning and then stacking it
+    // reversedStr.charAt(0) == 'c'
+    // reversedStr.substr(1) ==
+    // recursive step
+    else {
+      return reversedStringRecursion(reversedStr.substr(1)) + reversedStr.charAt(0);
+    }
+// https://www.freecodecamp.org/news/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb//
+/* 
+// Recursive
+
+Each call: str === "?"        	                  reverseString(str.subst(1))     + str.charAt(0)
+1st call – reverseString("cat")   will return   reverseString("at")           + "c"
+2nd call – reverseString("at")    will return   reverseString("t")            + "ac"
+3rd call – reverseString("t")     will return   reverseString("t")             + "tac"
+*/
+}
+
+console.log(reversedStringRecursion('cat'))
